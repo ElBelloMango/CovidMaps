c7ea71bb9c0ef9227f33fed5596af9c915b235d2
ElBelloMango <miguel.gallego@upb.edu.co>
2021-09-29 14:59:40 -0500

    Limpieza de código

--- ProyectoFinalTele/bdtesting.py
+++ ProyectoFinalTele/bdtesting.py
@@ -7,25 +7,33 @@ from dash import html
 import plotly.graph_objs as go
 import numpy as np
 from decimal import Decimal
- 
 
 app = Flask(__name__)
 db_path = 'ProyectoFinalTele/BD/CovidMaps.db'
-mapa = dash.Dash(__name__,server=app,routes_pathname_prefix='/mapa/')
-df = pd.read_excel('BaseDeDatosValleDeAburra100%realNoFake.xls')
+external_stylesheets = [
+    'https://codepen.io/chriddyp/pen/bWLwgP.css',
+    {
+        'href': 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css',
+        'rel': 'stylesheet',
+        'integrity': 'sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We',
+        'crossorigin': 'anonymous'
+    },
+    {
+        'href': 'templates/css/style.css',
+        'rel': 'stylesheet'
+    }
+]
+mapa = dash.Dash(__name__, server=app, routes_pathname_prefix='/mapa/',
+                 external_stylesheets=external_stylesheets, title="CovidMaps")
 cnx = sqlite3.connect(db_path)
 database = pd.read_sql_query("SELECT * FROM contagiados", cnx)
-data = pd.concat([database,df],axis=0)
-vectorLat = data['latitud']
-vectorLon = data['longitud']
 cnx.commit()
 cnx.close()
 
- 
 
 @app.route('/usuario', methods=["POST", "GET"])
 def home():
-   if request.method=="POST":
+    if request.method == "POST":
         nombre = request.form.get("nombre")
         apellido = request.form.get("apellidos")
         sexo = request.form.get("sexo")
@@ -36,164 +44,87 @@ def home():
         if corona == "SI":
             con = sqlite3.connect(db_path)
             cur = con.cursor()
-            cur.execute("INSERT INTO contagiados(nombre,apellidos,sexo,edad,email,id_localidad) VALUES('{}','{}','{}','{}','{}','{}')".format(nombre,apellido,sexo,edad,correo,localidad))
+            cur.execute("INSERT INTO contagiados(nombre,apellidos,sexo,edad,email,id_localidad) VALUES('{}','{}','{}','{}','{}','{}')".format(
+                nombre, apellido, sexo, edad, correo, localidad))
             con.commit()
             con.close()
         return redirect('/mapa')
 
- 
 
 @app.route('/mapa')
 def graficar():
-    df = pd.read_excel('BaseDeDatosValleDeAburra100%realNoFake.xls')
     cnx = sqlite3.connect(db_path)
     database = pd.read_sql_query("SELECT * FROM contagiados", cnx)
     cnx.commit()
     cnx.close()
-    data = pd.concat([database,df],axis=0)
-    vectorLat = data['latitud']
-    vectorLon = data['longitud']
     mapa.layout = html.Div([
-            html.H1('Covid 19 en el valle de Aburra'),
-        html.Button('Analizar Zonas', id='submit-val', n_clicks=0),
-        html.H1(id='test'),
-            html.Div(id='text-content'),
-           dcc.Graph(id='map', figure={
-                'data': [{
-                'lat': vectorLat,
-                        'lon': vectorLon,
-                'marker': {
-                            'color': 'blue',
-                            'size': 20,
-                            'opacity': 0.6
-                        },
-                        'customdata': 2,
-                        'type': 'scattermapbox'
-                   }],
-                'layout': {
-                        'mapbox': {
-                            'accesstoken': 'pk.eyJ1IjoibGVvbmFyZG9iZXRhbmN1ciIsImEiOiJjazlybGNiZWcwYjZ6M2dwNGY4MmY2eGpwIn0.EJjpR4klZpOHSfdm7Tsfkw',
-                    'center' :{
-                        'lat' : 6.242095,
-                        'lon' : -75.589626
-                    },
-                    'zoom' : 10,
-                    'style' : 'dark'
-                        },
-                        'hovermode': 'closest',
-                        'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0}
-                }
-            })
-    ])
+        html.H1('Covid 19 en el valle de Aburra'),
+        html.Button('Actualizar datos', id='submit-val',
+                    className="btn btn-outline-primary btn-lg btn-block", n_clicks=0),
+        html.Button('Zoom', id='zoom',
+                    className="btn btn-outline-warning btn-lg btn-block", n_clicks=0),
+        html.Td(),
+        html.Div(id='text-content'),
+        dcc.Graph(id='map'),
+    ], className="container")
+
+
 @mapa.callback(
     dash.dependencies.Output('map', 'figure'),
     [dash.dependencies.Input('submit-val', 'n_clicks')])
-def actualizar(clicks):
-    df = pd.read_excel('BaseDeDatosValleDeAburra100%realNoFake.xls')
+def actualizar(n_clicks):
     cnx = sqlite3.connect(db_path)
     database = pd.read_sql_query("SELECT * FROM contagiados", cnx)
     cnx.commit()
     cnx.close()
-    data = pd.concat([database,df],axis=0)
-    vectorLat = data['latitud']
-    vectorLon = data['longitud']
-    nororiental=0
-    noroccidental=0
-    suroriental=0
-    suroccidental=0
-    color = []
-    # for x in range(0,len(df['latitud'])):
-    #     if (Decimal(df['longitud'][x])>Decimal(-75.565780) and Decimal(df['latitud'][x])>Decimal(6.269979)):
-    #         nororiental = nororiental + 1
-    #     elif df['longitud'][x]>(-75.633870) and df['longitud'][x]<(-75.565780) and df['latitud'][x]>(6.269979) and df['latitud'][x]<(6.347253):
-    #         noroccidental = noroccidental + 1
-    #     elif df['longitud'][x]>(-75.565780) and df['longitud'][x]<(-75.495771) and df['latitud'][x]>(6.139561) and df['latitud'][x]<(6.269979):
-    #         suroriental = suroriental + 1
-    #     elif df['longitud'][x]>(-75.633870) and df['longitud'][x]<(-75.565780) and df['latitud'][x]>(6.139561) and df['latitud'][x]<(6.269979):
-    #         suroccidental = suroccidental + 1
-    # for x in range(0,len(database['latitud'])):
-    #     if (Decimal(database['longitud'][x])>Decimal(-75.565780) and Decimal(database['latitud'][x])>Decimal(6.269979)):
-    #         nororiental = nororiental + 1
-    #     elif database['longitud'][x]>-75.633870 and database['longitud'][x]<-75.565780 and database['latitud'][x]>6.269979 and database['latitud'][x]<6.347253:
-    #         noroccidental = noroccidental + 1
-    #     elif database['longitud'][x]>-75.565780 and database['longitud'][x]<-75.495771 and database['latitud'][x]>6.139561 and database['latitud'][x]<6.269979:
-    #         suroriental = suroriental + 1
-    #     elif database['longitud'][x]>-75.633870 and database['longitud'][x]<-75.565780 and database['latitud'][x]>6.139561 and database['latitud'][x]<6.269979:
-    #         suroccidental = suroccidental + 1
-    figure={
-                'data': [{
-                'lat': vectorLat,
-                        'lon': vectorLon,
-                'marker': {
-                            'color': 'green',
-                            'size': 20,
-                            'opacity': 0.6
-                        },
-                        'customdata': 2,
-                        'type': 'scattermapbox'
-                }],
-                'layout': {
-                        'mapbox': {
-                            'accesstoken': 'pk.eyJ1IjoibGVvbmFyZG9iZXRhbmN1ciIsImEiOiJjazlybGNiZWcwYjZ6M2dwNGY4MmY2eGpwIn0.EJjpR4klZpOHSfdm7Tsfkw',
-                    'center' :{
-                        'lat' : 6.242095,
-                        'lon' : -75.589626
-                    },
-                    'zoom' : 10,
-                    'style' : 'dark'
-                        },
-                        'hovermode': 'closest',
-                        'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0}
-                }
-            }
+    # figure = go.Scattermapbox(data=None,layout = None) Util para luego, clase plotly con dash
+    figure = {
+        'data': [{
+            # 'lat': vectorLat,
+            #         'lon': vectorLon, #Datos de posiciones
+            'marker': {
+                'color': 'green',
+                'size': 20,
+                'opacity': 0.6
+            },
+            'customdata': 2,
+            'type': 'scattermapbox'
+        }],
+        'layout': {
+            'mapbox': {
+                'accesstoken': 'pk.eyJ1IjoibGVvbmFyZG9iZXRhbmN1ciIsImEiOiJjazlybGNiZWcwYjZ6M2dwNGY4MmY2eGpwIn0.EJjpR4klZpOHSfdm7Tsfkw',
+                'center': {
+                    'lat': 6.242095,
+                    'lon': -75.589626
+                },
+                'zoom': 10,
+                'style': 'dark'
+            },
+            'hovermode': 'closest',
+            'margin': {'l': 0, 'r': 0, 'b': 0, 't': 0}
+        }
+    }
     return figure
-@mapa.callback(
-    dash.dependencies.Output('test', 'children'),
-    [dash.dependencies.Input('submit-val', 'n_clicks')])
-def sendata(clicks):
-    df = pd.read_excel('BaseDeDatosValleDeAburra100%realNoFake.xls')
-    cnx = sqlite3.connect(db_path)
-    database = pd.read_sql_query("SELECT * FROM contagiados", cnx)
-    cnx.commit()
-    cnx.close()
-    nororiental=0
-    noroccidental=0
-    suroriental=0
-    suroccidental=0
-    color = []
-    # for x in range(0,len(df['latitud'])):
-    #     if (Decimal(df['longitud'][x])>Decimal(-75.565780) and Decimal(df['latitud'][x])>Decimal(6.269979)):
-    #         nororiental = nororiental + 1
-    #     elif df['longitud'][x]<(-75.565780) and df['latitud'][x]>(6.269979):
-    #         noroccidental = noroccidental + 1
-    #     elif df['longitud'][x]>(-75.565780) and df['latitud'][x]<(6.269979):
-    #         suroriental = suroriental + 1
-    #     elif df['longitud'][x]<(-75.565780) and df['latitud'][x]<(6.269979):
-    #         suroccidental = suroccidental + 1
-    # for x in range(0,len(database['latitud'])):
-    #     if (Decimal(database['longitud'][x])>Decimal(-75.565780) and Decimal(database['latitud'][x])>Decimal(6.269979)):
-    #         nororiental = nororiental + 1
-    #     elif database['longitud'][x]<-75.565780 and database['latitud'][x]>6.269979:
-    #         noroccidental = noroccidental + 1
-    #     elif database['longitud'][x]>-75.565780 and database['latitud'][x]<6.269979:
-    #         suroriental = suroriental + 1
-    #     elif database['longitud'][x]<-75.565780 and database['latitud'][x]<6.269979:
-    #         suroccidental = suroccidental + 1
-    return "Zona nororiental: ",nororiental," ||Zona noroccidental: ",noroccidental," ||Zona suroriental: ",suroriental," ||Zona suroccidental: ",suroccidental
-@app.route('/', methods =["GET"])
+
+
+# @mapa.callback(
+#     dash.dependencies.Output('test', 'children'),
+#     [dash.dependencies.Input('submit-val', 'n_clicks')])
+
+# def sendata(clicks):
+#     return "Zona nororiental: ", nororiental, " ||Zona noroccidental: ", noroccidental, " ||Zona suroriental: ", suroriental, " ||Zona suroccidental: ", suroccidental
+
+
+@app.route('/', methods=["GET"])
 def logueo():
-    data = pd.concat([database,df],axis=0)
-    vectorLat = data['latitud']
-    vectorLon = data['longitud']
     cnx = sqlite3.connect('ProyectoFinalTele/BD/CovidMaps.db')
     localidades = pd.read_sql_query("SELECT * FROM localidades", cnx)
     cnx.commit()
     cnx.close()
     localidades = localidades.to_dict(orient='records')
-    return render_template("Rework.html",localidades=localidades)
+    return render_template("Rework.html", localidades=localidades)
 
- 
 
 if __name__ == '__main__':
     graficar()
-    mapa.run_server(debug = True, host = '0.0.0.0', port = 80)
+    mapa.run_server(debug=True, host='0.0.0.0', port=80)
